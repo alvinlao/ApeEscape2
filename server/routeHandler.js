5872f50feee48d49d2ae44b488656456590aebb8
Niko Savas <nikothevlan@hotmail.com>
2015-08-08 20:50:07 -0400

    Added leaderboards

--- server/routeHandler.js
+++ server/routeHandler.js
@@ -1,7 +1,14 @@
 var express = require("express");
 
 //Import custom endpoints
-var highscore = require("./highscore");
+var highscore = require("./interface/highscore");
+
+var routes = {
+    GET: {},
+    POST: {},
+    PUT: {},
+    DELETE: {}
+};
 
 /*
  * Sets up the endpoints for the server
@@ -9,38 +16,31 @@ var highscore = require("./highscore");
 var routeHandler = function() {
     var self = this;
 
-    self.routes = {
-        GET: {},
-        POST: {},
-        PUT: {},
-        DELETE: {}
-    };
-
     //Add the endpoints
-    self.routes.GET["/leaderboards"] = highscore.getHighscores;
+    routes.GET["/leaderboards"] = highscore.getHighscores;
 }
 
 function setupEndpoints(server) {
-
-    var handler = new routeHandler();
-
     //HTTP GET
-    for (var endpoint in handler) {
-        server.get(endpoint, handler.routes.GET[endpoint]);
+    for (var endpoint in routes.GET) {
+        console.log(endpoint);
+        server.get(endpoint, routes.GET[endpoint]);
     }
 
     //HTTP POST
-    for (var endpoint in handler) {
-        server.post(endpoint, handler.routes.POST[endpoint]);
+    for (var endpoint in routes.POST) {
+        server.post(endpoint, routes.POST[endpoint]);
     }
 
     //HTTP PUT
-    for (var endpoint in handler) {
-        server.put(endpoint, handler.routes.PUT[endpoint]);
+    for (var endpoint in routes.PUT) {
+        server.put(endpoint, routes.PUT[endpoint]);
     }
 
     //HTTP DELETE
-    for (var endpoint in handler) {
-        server.delete(endpoint, handler.routes.DELETE[endpoint]);
+    for (var endpoint in routes.DELETE) {
+        server.delete(endpoint, routes.DELETE[endpoint]);
     }
 }
+
+exports.setupEndpoints = setupEndpoints;
