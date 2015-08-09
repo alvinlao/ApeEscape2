cb676dc945e6ad85bbb43c04f41b131740396bf7
Alvin Lao <alvin.lao.is@gmail.com>
2015-08-09 12:43:55 -0700

    Fix broken paths

---
+++ client/js/game/entities/Entity.js
@@ -0,0 +1,4 @@
+function Entity() {
+}
+
+Entity.prototype = Phaser.Sprite;
--- client/js/game/preload.js
+++ client/js/game/preload.js
@@ -1,9 +1,9 @@
 function preload() {
 	game.stage.backgroundColor = '#85b5e1';
 
-    game.load.tilemap('level1', 'js/game/level1.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.image('tiles', 'js/game/tilesheet.png');
-    game.load.image('player', 'js/game/phaser-dude.png');
+    game.load.tilemap('level1', 'js/game/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/tilesheet.png');
+    game.load.image('player', 'assets/phaser-dude.png');
 
 
 }
